a69848f374f346a9ec160a0ec9fac5f8db071dd9
crenaz <crenaz@gmail.com>
2022-11-25 19:44:34 -0400

    Making changes to site copy

--- pages/hosting.js
+++ pages/hosting.js
@@ -3,15 +3,15 @@ import CardFeaturesHosting from '../components/elements/card-features-hosting'
 import ServicesHosting from '../components/elements/services-hosting'
 import Header from '../components/header/Header'
 import PageHeader from '../components/header/page-header'
-export default function Hosting() {
+export default function Purring() {
   return (
     <>
       <Head>
-        <title>Powerful Web Hosting</title>
+        <title>Powerful Real Purring</title>
       </Head>
       <header className="header header-page">
         <Header />
-        <PageHeader title="Powerful Web Hosting" />
+        <PageHeader title="Powerful Real Purring" />
       </header>
       <main className='main'>
         <div className='container'>
